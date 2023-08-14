d9ab9e573be9a520879703c736ef0971a5cf54a8
poshdy <roshdy2810@gmail.com>
2023-08-14 08:37:51 -0700

    m

--- app/(auth)/callback/route.ts
+++ app/(auth)/callback/route.ts
@@ -2,6 +2,7 @@ import { NextResponse , NextRequest } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
+export const dynamic = 'force-dynamic'
 export  async function GET(req:NextRequest){
     const requestUrl = new URL(req.url)
   
