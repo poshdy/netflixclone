8b23db13c1253301e1ca56da74914612d460ccb8
poshdy <roshdy2810@gmail.com>
2023-08-14 07:31:47 -0700

    m

--- app/(auth)/callback/route.ts
+++ app/(auth)/callback/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse , NextRequest } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import 'server-only'
 export  async function GET(req:NextRequest){
     const supabase = createRouteHandlerClient({cookies})
     const {searchParams} = new URL(req.url)
