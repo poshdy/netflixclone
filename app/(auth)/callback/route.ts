0dc1c63e69248077194c257b2201618598dd8fa3
poshdy <roshdy2810@gmail.com>
2023-08-14 07:49:53 -0700

    fixing route

--- app/(auth)/callback/route.ts
+++ app/(auth)/callback/route.ts
@@ -1,13 +1,14 @@
 import { NextResponse , NextRequest } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-'use server'
+
 export  async function GET(req:NextRequest){
-    const supabase = createRouteHandlerClient({cookies})
-    const {searchParams} = new URL(req.url)
-    const code = searchParams.get('code')
+    const requestUrl = new URL(req.url)
+  
+    const code = requestUrl.searchParams.get('code')
     if(code){
+        const supabase = createRouteHandlerClient({cookies})
         await supabase.auth.exchangeCodeForSession(code)
     }
-    return NextResponse.redirect(new URL('/', req.url))
+    return NextResponse.redirect(requestUrl.origin)
 }
